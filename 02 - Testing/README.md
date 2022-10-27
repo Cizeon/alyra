ed540922e3a232074745600460d2af038c6199a6
Cizeon <admin@cizeon.xyz>
2022-10-27 17:14:38 +0200

    Refactor. Adding gas reporter. Updating the README.

--- 02 - Testing/README.md
+++ 02 - Testing/README.md
@@ -10,11 +10,13 @@ Requested by [Alyra](https://alyra.fr/) for first project request of the Rinkeby
 
 For the previous course, I used Hardhat. This time is used Truffle to match with the requirements.
 
-The script is testing 26 cases:
+The script is testing 31 cases:
 
-- The overall workflow.
-- Error cases (contract ownership, cannot add empty proposals, cannot vote twice, ...).
-- The global voting process.
+- Ownership
+- Registration state
+- Proposal state
+- Voting state
+- Tally state
 
 # Testing
 
@@ -25,40 +27,76 @@ Using network 'development'.
 
 Compiling your contracts...
 ===========================
-> Everything is up to date, there is nothing to compile.
-
+> Compiling ./contracts/Voting.sol
+> Artifacts written to /var/folders/dx/klncq4mn26ncyy1g5tsdlkq80000gn/T/test--72879-D5negp5JtKLv
+> Compiled successfully using:
+   - solc: 0.8.13+commit.abaa5c0e.Emscripten.clang
 
   Contract: Voting
-    Owner tests
-      ✔ sets the correct owner
-      ✔ forbids transfer ownership if not owner (101ms)
-    Workflow Status tests
-      ✔ should not allow to tally votes before end of the voting session
-      ✔ should not allow to end votes before it started
-      ✔ should not allow to start votes before end of proposal phase
-      ✔ should not allow to end proposals before it started
-    Voting process tests
-      ✔ should test voter registration emit the proper event
-      ✔ should not add a voter twice
-      ✔ should check that only the owner can add voters
-      ✔ should check only voters can call getVoter()
-      ✔ should properly register a voter
-      ✔ should check proposals registration phase
-      ✔ should check that only owners can start the proposal phase
-      ✔ should emit the proper event when starting the proposal phase
-      ✔ should check that only voters can submit a proposal
-      ✔ should add a new proposal with the proper event (40ms)
-      ✔ should not add an empty proposal
-      ✔ should emit the proper event when stopping the proposal phase
-      ✔ should block votes before voting phase
-      ✔ should emit the proper event when starting the voting phase
-      ✔ should block votes from not allowed addresses
-      ✔ should emit the proper event when voting (49ms)
-      ✔ should emit the proper event when stoping the voting phase
-      ✔ should check that onlyOwner can tally votes
-      ✔ should emit the proper event when tally votes
-      ✔ should return to good winner
+    Testing ownership
+      ✓ sets the correct owner (5ms)
+      ✓ forbids transfer ownership if not owner (POV hacker) (114ms)
+    Testing registration state
+      ✓ forbids anyone from adding voters (POV hacker) (7ms)
+      ✓ should test voter registration to emit the proper event (13ms, 50220 gas)
+      ✓ should properly register a voter (POV voter1) (18ms, 50220 gas)
+      ✓ block adding a voter twice (20ms, 50220 gas)
+      ✓ should check only voters can call getVoter() (POV hacker) (18ms, 50220 gas)
+    Testing proposal state
+      ✓ forbids adding a proposal before proposal phase (POV voter1) (7ms)
+      ✓ forbids anyone from starting proposal phase (POV hacker) (8ms)
+      ✓ should emit the proper event when starting the proposal phase (70ms, 94840 gas)
+      ✓ should check that only voters can submit a proposal (POV hacker) (20ms, 94840 gas)
+      ✓ should add a new proposal with the proper event (POV voter1) (34ms, 153928 gas)
+      ✓ should not add an empty proposal (POV voter1) (19ms, 94840 gas)
+      ✓ returns the correct proposal by id (POV voter1) (29ms, 153928 gas)
+      ✓ forbids non voter to access a proposal (POV hacker) (38ms, 153928 gas)
+      ✓ should emit the proper event when stopping the proposal phase (29ms, 125439 gas)
+    Testing voting state
+      ✓ blocks votes before voting phase (POV voter1) (10ms)
+      ✓ emits the proper event when starting the voting phase (13ms, 30554 gas)
+      ✓ blocks votes from not allowed addresses (POV hacker) (17ms, 30554 gas)
+      ✓ emits the proper event when voting (POV voter1) (25ms, 108567 gas)
+      ✓ upgrade vote count on vote (POV voter1) (29ms, 108567 gas)
+      ✓ blocks voting twice (POV voter1) (30ms, 108567 gas)
+      ✓ blocks voting for an unknown proposal (14ms, 30554 gas)
+      ✓ should emit the proper event when stoping the voting phase (20ms, 61087 gas)
+    Testing tally state
+      ✓ blocks starting the proposal phase (5ms)
+      ✓ blocks ending the proposal phase (6ms)
+      ✓ blocks starting voting session a second time (4ms)
+      ✓ blocks tally votes before voting session is over (5ms)
+      ✓ should check that onlyOwner can tally votes (POV hacker) (15ms, 30533 gas)
+      ✓ should emit the proper event when tally votes (31ms, 94098 gas)
+      ✓ sets to proper winner (28ms, 94098 gas)
 
+·------------------------------------------|----------------------------|-------------|----------------------------·
+|   Solc version: 0.8.13+commit.abaa5c0e   ·  Optimizer enabled: false  ·  Runs: 200  ·  Block limit: 6718946 gas  │
+···········································|····························|·············|·····························
+|  Methods                                                                                                         │
+·············|·····························|··············|·············|·············|··············|··············
+|  Contract  ·  Method                     ·  Min         ·  Max        ·  Avg        ·  # calls     ·  eur (avg)  │
+·············|·····························|··············|·············|·············|··············|··············
+|  Voting    ·  addProposal                ·           -  ·          -  ·      59088  ·          28  ·          -  │
+·············|·····························|··············|·············|·············|··············|··············
+|  Voting    ·  addVoter                   ·           -  ·          -  ·      50220  ·          58  ·          -  │
+·············|·····························|··············|·············|·············|··············|··············
+|  Voting    ·  endProposalsRegistering    ·           -  ·          -  ·      30599  ·          18  ·          -  │
+·············|·····························|··············|·············|·············|··············|··············
+|  Voting    ·  endVotingSession           ·           -  ·          -  ·      30533  ·           6  ·          -  │
+·············|·····························|··············|·············|·············|··············|··············
+|  Voting    ·  setVote                    ·       60913  ·      78013  ·      70176  ·          24  ·          -  │
+·············|·····························|··············|·············|·············|··············|··············
+|  Voting    ·  startProposalsRegistering  ·           -  ·          -  ·      94840  ·          25  ·          -  │
+·············|·····························|··············|·············|·············|··············|··············
+|  Voting    ·  startVotingSession         ·           -  ·          -  ·      30554  ·          17  ·          -  │
+·············|·····························|··············|·············|·············|··············|··············
+|  Voting    ·  tallyVotes                 ·           -  ·          -  ·      63565  ·           3  ·          -  │
+·············|·····························|··············|·············|·············|··············|··············
+|  Deployments                             ·                                          ·  % of limit  ·             │
+···········································|··············|·············|·············|··············|··············
+|  Voting                                  ·           -  ·          -  ·    1970027  ·      29.3 %  ·          -  │
+·------------------------------------------|--------------|-------------|-------------|--------------|-------------·
 
-  26 passing (708ms)
+  31 passing (16s)
 ```
--- 02 - Testing/test/test.js
+++ 02 - Testing/test/test.js
@@ -1,108 +1,116 @@
 const { BN, expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
-const constants = require('@openzeppelin/test-helpers/src/constants');
 
 const Voting = artifacts.require('Voting');
 
+/**
+ * @dev This is the testing script for the Voting contract.
+ * Tests multiple scenarios from ownership, to normal voting process, to malicous activity.
+ */
+
 contract('Voting', (accounts) => {
   let owner = accounts[0];
+  let voter1 = accounts[1];
+  let voter2 = accounts[2];
+  let hacker = accounts[4];
   let votingInstance;
 
-  beforeEach(async () => {
-    votingInstance = await Voting.deployed();
-  });
+  /**
+   * @dev WorkflowStatus enum values.
+   */
+
+  const WorkflowStatus = {
+    RegisteringVoters: 0,
+    ProposalsRegistrationStarted: 1,
+    ProposalsRegistrationEnded: 2,
+    VotingSessionStarted: 3,
+    VotingSessionEnded: 4,
+    VotesTallied: 5,
+  };
 
   /**
    * @dev Check ownership of the contract.
    */
 
-  describe('Owner tests', async () => {
+  describe('Testing ownership', async () => {
+    beforeEach(async () => {
+      votingInstance = await Voting.new();
+    });
+
     it('sets the correct owner', async () => {
       const contractOwner = await votingInstance.owner();
       expect(contractOwner).to.be.equal(owner);
     });
 
-    it('forbids transfer ownership if not owner', async () => {
+    it('forbids transfer ownership if not owner (POV hacker)', async () => {
       await expectRevert(
-        votingInstance.transferOwnership(accounts[1], { from: accounts[1] }),
+        votingInstance.transferOwnership(hacker, { from: hacker }),
         'Ownable: caller is not the owner',
       );
     });
   });
 
   /**
-   * @dev Check that you cannot call some functions during the wrong phase.
+   * @dev Test registration state.
    */
 
-  describe('Workflow Status tests', async () => {
-    it('blocks tally votes before end of the voting session', async () => {
-      await expectRevert(votingInstance.tallyVotes(), 'Current status is not voting session ended');
+  describe('Testing registration state', async () => {
+    beforeEach(async () => {
+      votingInstance = await Voting.new();
     });
 
-    it('blocks ending votes before it started', async () => {
-      await expectRevert(votingInstance.endVotingSession(), 'Voting session havent started yet');
-    });
-
-    it('blocks starting votes before end of proposal phase', async () => {
+    it('forbids anyone from adding voters (POV hacker)', async () => {
       await expectRevert(
-        votingInstance.startVotingSession(),
-        'Registering proposals phase is not finished',
+        votingInstance.addVoter(hacker, { from: hacker }),
+        'Ownable: caller is not the owner',
       );
     });
 
-    it('blocks ending proposals before it started', async () => {
-      await expectRevert(
-        votingInstance.endProposalsRegistering(),
-        'Registering proposals havent started yet',
-      );
-    });
-  });
-
-  /**
-   * @dev Test the voting process.
-   */
-
-  describe('Voting process tests', async () => {
-    it('should test voter registration emit the proper event', async () => {
-      let result = await votingInstance.addVoter(accounts[1]);
+    it('should test voter registration to emit the proper event', async () => {
+      let result = await votingInstance.addVoter(voter1);
       expectEvent(result, 'VoterRegistered', {
-        voterAddress: accounts[1],
+        voterAddress: voter1,
       });
     });
 
-    it('should not add a voter twice', async () => {
-      await expectRevert(votingInstance.addVoter(accounts[1]), 'Already registered');
-    });
-
-    it('should check that only the owner can add voters', async () => {
-      await expectRevert(
-        votingInstance.addVoter(accounts[3], { from: accounts[1] }),
-        'Ownable: caller is not the owner',
-      );
-    });
-
-    it('should check only voters can call getVoter()', async () => {
-      await expectRevert(votingInstance.getVoter(accounts[1]), "You're not a voter");
-    });
-
-    it('should properly register a voter', async () => {
-      result = await votingInstance.getVoter(accounts[1], { from: accounts[1] });
+    it('should properly register a voter (POV voter1)', async () => {
+      let result = await votingInstance.addVoter(voter1);
+      result = await votingInstance.getVoter(voter1, { from: voter1 });
       expect(result.isRegistered).to.equal(true);
-
-      /* Adding new voters from future tests. */
-      await votingInstance.addVoter(accounts[2]);
     });
 
-    it('should check proposals registration phase', async () => {
+    it('block adding a voter twice', async () => {
+      await votingInstance.addVoter(voter1);
+      await expectRevert(votingInstance.addVoter(voter1), 'Already registered');
+    });
+
+    it('should check only voters can call getVoter() (POV hacker)', async () => {
+      await votingInstance.addVoter(voter1);
+      await expectRevert(votingInstance.getVoter(voter1, { from: hacker }), "You're not a voter");
+    });
+  });
+
+  /**
+   * @dev Test the proposal state.
+   */
+
+  describe('Testing proposal state', async () => {
+    beforeEach(async () => {
+      votingInstance = await Voting.new();
+      await votingInstance.addVoter(voter1);
+      await votingInstance.addVoter(voter2);
+    });
+
+    it('forbids adding a proposal before proposal phase (POV voter1)', async () => {
       await expectRevert(
-        votingInstance.addProposal('proposal 1', { from: accounts[1] }),
+        votingInstance.addProposal('proposal 1', { from: voter1 }),
         'Proposals are not allowed yet',
       );
     });
 
-    it('should check that only owners can start the proposal phase', async () => {
+    it('forbids anyone from starting proposal phase (POV hacker)', async () => {
       await expectRevert(
-        votingInstance.startProposalsRegistering({ from: accounts[1] }),
+        votingInstance.startProposalsRegistering({ from: hacker }),
         'Ownable: caller is not the owner',
       );
     });
@@ -110,96 +118,194 @@ contract('Voting', (accounts) => {
     it('should emit the proper event when starting the proposal phase', async () => {
       const result = await votingInstance.startProposalsRegistering();
       await expectEvent(result, 'WorkflowStatusChange', {
-        previousStatus: BN(0),
-        newStatus: BN(1),
+        previousStatus: BN(WorkflowStatus.RegisteringVoters),
+        newStatus: BN(WorkflowStatus.ProposalsRegistrationStarted),
       });
     });
 
-    it('should check that only voters can submit a proposal', async () => {
-      await expectRevert(votingInstance.addProposal('proposal 1'), "You're not a voter");
+    it('should check that only voters can submit a proposal (POV hacker)', async () => {
+      await votingInstance.startProposalsRegistering();
+      await expectRevert(
+        votingInstance.addProposal('proposal 1', { from: hacker }),
+        "You're not a voter",
+      );
     });
 
-    it('should add a new proposal with the proper event', async () => {
-      let result = await votingInstance.addProposal('proposal 1', { from: accounts[1] });
+    it('should add a new proposal with the proper event (POV voter1)', async () => {
+      await votingInstance.startProposalsRegistering();
+      let result = await votingInstance.addProposal('proposal 1', { from: voter1 });
       expectEvent(result, 'ProposalRegistered', {
         proposalId: BN(1),
       });
-
-      await votingInstance.addProposal('proposal 2', { from: accounts[2] });
     });
 
-    it('should not add an empty proposal', async () => {
+    it('should not add an empty proposal (POV voter1)', async () => {
+      await votingInstance.startProposalsRegistering();
       await expectRevert(
-        votingInstance.addProposal('', { from: accounts[1] }),
+        votingInstance.addProposal('', { from: voter1 }),
         'Vous ne pouvez pas ne rien proposer',
       );
     });
 
-    it('should emit the proper event when stopping the proposal phase', async () => {
-      const result = await votingInstance.endProposalsRegistering();
-      await expectEvent(result, 'WorkflowStatusChange', {
-        previousStatus: BN(1),
-        newStatus: BN(2),
-      });
+    it('returns the correct proposal by id (POV voter1)', async () => {
+      await votingInstance.startProposalsRegistering();
+      await votingInstance.addProposal('proposal 1', { from: voter1 });
+      let result = await votingInstance.getOneProposal(1, { from: voter1 });
+      expect(result.description).to.be.equal('proposal 1');
     });
 
-    it('should block votes before voting phase', async () => {
+    it('forbids non voter to access a proposal (POV hacker)', async () => {
+      await votingInstance.startProposalsRegistering();
+      await votingInstance.addProposal('proposal 1', { from: voter1 });
+      await expectRevert(votingInstance.getOneProposal(1, { from: hacker }), "You're not a voter");
+    });
+
+    it('should emit the proper event when stopping the proposal phase', async () => {
+      await votingInstance.startProposalsRegistering();
+      const result = await votingInstance.endProposalsRegistering();
+      await expectEvent(result, 'WorkflowStatusChange', {
+        previousStatus: BN(WorkflowStatus.ProposalsRegistrationStarted),
+        newStatus: BN(WorkflowStatus.ProposalsRegistrationEnded),
+      });
+    });
+  });
+
+  /**
+   * @dev Test the voting state.
+   */
+
+  describe('Testing voting state', async () => {
+    beforeEach(async () => {
+      votingInstance = await Voting.new();
+      await votingInstance.addVoter(voter1);
+      await votingInstance.addVoter(voter2);
+      await votingInstance.startProposalsRegistering();
+      await votingInstance.addProposal('proposal 1', { from: voter1 });
+      await votingInstance.endProposalsRegistering();
+    });
+
+    it('blocks votes before voting phase (POV voter1)', async () => {
       await expectRevert(
-        votingInstance.setVote(1, { from: accounts[1] }),
+        votingInstance.setVote(1, { from: voter1 }),
         'Voting session havent started yet',
       );
     });
 
-    it('should emit the proper event when starting the voting phase', async () => {
+    it('emits the proper event when starting the voting phase', async () => {
       const result = await votingInstance.startVotingSession();
       await expectEvent(result, 'WorkflowStatusChange', {
-        previousStatus: BN(2),
-        newStatus: BN(3),
+        previousStatus: BN(WorkflowStatus.ProposalsRegistrationEnded),
+        newStatus: BN(WorkflowStatus.VotingSessionStarted),
       });
     });
 
-    it('should block votes from not allowed addresses', async () => {
-      await expectRevert(votingInstance.setVote(1), "You're not a voter");
+    it('blocks votes from not allowed addresses (POV hacker)', async () => {
+      await votingInstance.startVotingSession();
+      await expectRevert(votingInstance.setVote(1, { from: hacker }), "You're not a voter");
     });
 
-    it('should emit the proper event when voting', async () => {
-      let result = await votingInstance.setVote(1, { from: accounts[1] });
-      await expectEvent(result, 'Voted', {
-        voter: accounts[1],
-        proposalId: BN(1),
-      });
+    it('emits the proper event when voting (POV voter1)', async () => {
+      await votingInstance.startVotingSession();
 
-      result = await votingInstance.setVote(1, { from: accounts[2] });
+      let result = await votingInstance.setVote(1, { from: voter1 });
       await expectEvent(result, 'Voted', {
-        voter: accounts[2],
+        voter: voter1,
         proposalId: BN(1),
       });
+    });
+
+    it('upgrade vote count on vote (POV voter1)', async () => {
+      await votingInstance.startVotingSession();
+      let currentVoteCount = (await votingInstance.getOneProposal(1, { from: voter1 })).voteCount;
+      await votingInstance.setVote(1, { from: voter1 });
+      let newVoteCount = (await votingInstance.getOneProposal(1, { from: voter1 })).voteCount;
+      expect(newVoteCount).to.be.bignumber.equal(BN(currentVoteCount + 1));
+    });
+
+    it('blocks voting twice (POV voter1)', async () => {
+      await votingInstance.startVotingSession();
+      await votingInstance.setVote(1, { from: voter1 });
+      await expectRevert(votingInstance.setVote(1, { from: voter1 }), 'You have already voted');
+    });
+
+    it('blocks voting for an unknown proposal', async () => {
+      await votingInstance.startVotingSession();
+      await expectRevert(votingInstance.setVote(2, { from: voter1 }), 'Proposal not found');
     });
 
     it('should emit the proper event when stoping the voting phase', async () => {
+      await votingInstance.startVotingSession();
       const result = await votingInstance.endVotingSession();
       await expectEvent(result, 'WorkflowStatusChange', {
-        previousStatus: BN(3),
-        newStatus: BN(4),
+        previousStatus: BN(WorkflowStatus.VotingSessionStarted),
+        newStatus: BN(WorkflowStatus.VotingSessionEnded),
       });
     });
+  });
 
-    it('should check that onlyOwner can tally votes', async () => {
+  /**
+   * @dev Testing tally state.
+   */
+
+  describe('Testing tally state', async () => {
+    beforeEach(async () => {
+      votingInstance = await Voting.new();
+      await votingInstance.addVoter(voter1);
+      await votingInstance.addVoter(voter2);
+      await votingInstance.startProposalsRegistering();
+      await votingInstance.addProposal('proposal 1', { from: voter1 });
+      await votingInstance.addProposal('proposal 2', { from: voter2 });
+      await votingInstance.endProposalsRegistering();
+      await votingInstance.startVotingSession();
+      await votingInstance.setVote(1, { from: voter1 });
+      await votingInstance.setVote(1, { from: voter2 });
+    });
+
+    it('blocks starting the proposal phase', async () => {
       await expectRevert(
-        votingInstance.tallyVotes({ from: accounts[1] }),
+        votingInstance.startProposalsRegistering(),
+        'Registering proposals cant be started now',
+      );
+    });
+
+    it('blocks ending the proposal phase', async () => {
+      await expectRevert(
+        votingInstance.endProposalsRegistering(),
+        'Registering proposals havent started yet',
+      );
+    });
+
+    it('blocks starting voting session a second time', async () => {
+      await expectRevert(
+        votingInstance.startVotingSession(),
+        'Registering proposals phase is not finished',
+      );
+    });
+
+    it('blocks tally votes before voting session is over', async () => {
+      await expectRevert(votingInstance.tallyVotes(), 'Current status is not voting session ended');
+    });
+
+    it('should check that onlyOwner can tally votes (POV hacker)', async () => {
+      await votingInstance.endVotingSession();
+      await expectRevert(
+        votingInstance.tallyVotes({ from: hacker }),
         'Ownable: caller is not the owner',
       );
     });
 
     it('should emit the proper event when tally votes', async () => {
+      await votingInstance.endVotingSession();
       const result = await votingInstance.tallyVotes();
       await expectEvent(result, 'WorkflowStatusChange', {
-        previousStatus: BN(4),
-        newStatus: BN(5),
+        previousStatus: BN(WorkflowStatus.VotingSessionEnded),
+        newStatus: BN(WorkflowStatus.VotesTallied),
       });
     });
 
-    it('should return to good winner', async () => {
+    it('sets to proper winner', async () => {
+      await votingInstance.endVotingSession();
+      const result = await votingInstance.tallyVotes();
       const winningProposalID = await votingInstance.winningProposalID();
       expect(winningProposalID).to.be.bignumber.equal(BN(1));
     });
